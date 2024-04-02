002d1d290cf5985a340b8f571ef24fd112103c32
kashinathchormale01 <chormalekashinath01@gmail.com>
2024-04-02 19:00:34 +0530

    Employee Exit form design

---
+++ src/component/EmployeeExitPage/EmployeeExitPage.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Grid, Typography, TextField } from '@mui/material';
+import { InputField, SelectField, DatePickerField, TextareaField } from '../../global/FormFields';
+
+const genderlist = [
+    {
+      value: '1',
+      label: 'Male'
+    },
+    {
+      value: '2',
+      label: 'Female'
+    },
+    {
+      value: '3',
+      label: 'Other'
+    }
+  ];
+
+  const nationalitylist = [
+    {
+      value: '0',
+      label: 'India'
+    },
+    {
+        value: '1',
+        label: 'USA'
+    }
+  ];
+
+  const educationLevellist = [
+    {
+        value: undefined,
+        label: 'None'
+    },
+    {
+      value: '1',
+      label: 'BA'
+    },
+    {
+        value: '2',
+        label: 'BCOM'
+    },
+    {
+        value: '3',
+        label: 'HSC'
+    }
+  ];
+
+export default function EmpExitForm(props) {
+    // const [countryName, setCountryName] = React.useState("India");
+    const {
+        formField: {
+            empSelect,
+            empDOJ,
+            empServiceBookNumber,
+            empDesignation,
+            dateOfExit,
+            exitRemark,
+        }
+      } = props;
+      return (
+        <React.Fragment>
+            <Grid mt={0} mb={2} container spacing={3}>
+            <Grid item xs={12} sm={6}>
+                <InputField name={empServiceBookNumber} label={empServiceBookNumber} value={empServiceBookNumber} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <InputField name={empDesignation} label={empDesignation} value={empDesignation} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <InputField name={dateOfExit} label={dateOfExit} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextareaField name={exitRemark} label={exitRemark} value={exitRemark} fullWidth />
+            </Grid>
+            </Grid>
+          </React.Fragment>
+      );
+}
--- src/global/common/Routing.js
+++ src/global/common/Routing.js
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import EmployeeRegister from '../../component/EmployeeRegister';
 import LoginPage from '../../component/Login';
 import Dashboard from '../../component/Dashboard';
+import EmpExitForm from '../../component/EmployeeExitPage/EmployeeExitPage';
 
 
 const Routing = () => {
@@ -10,6 +11,7 @@ const Routing = () => {
     <>
       <Routes>
         <Route path="/employee-register" exact element={<EmployeeRegister />} />
+        <Route path="/employee-exit" exact element={<EmpExitForm />} />
         {/* Redirect for login page */}
         <Route path="/login" exact element={<LoginPage />} />
         <Route path="/dashboard" exact element={<Dashboard />} />
