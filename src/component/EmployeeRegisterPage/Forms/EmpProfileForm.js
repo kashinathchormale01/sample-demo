9ca1e98e210c4be7c4feed13895751a77582b72f
kashinathchormale01 <chormalekashinath01@gmail.com>
2024-04-05 21:39:47 +0530

    Emp reg datepicker inprogress

--- src/component/EmployeeRegisterPage/Forms/EmpProfileForm.js
+++ src/component/EmployeeRegisterPage/Forms/EmpProfileForm.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, Typography, TextField } from '@mui/material';
 import { InputField, SelectField, DatePickerField } from '../../../global/FormFields';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import moment from 'moment';
 
 const genderlist = [
     {
@@ -46,43 +48,65 @@ export default function EmpProfileForm(props) {
       } = props;
       return (
         <React.Fragment>
-          <Typography mt={4} variant="h6" gutterBottom>Profile Form</Typography>
+          <Typography mt={4} variant="h6" gutterBottom>
+            Profile Form
+          </Typography>
           <Grid mt={0} mb={2} container spacing={3}>
             <Grid item xs={12} sm={6}>
-                <InputField name={firstName.name} label={firstName.label} fullWidth />
+              <InputField
+                name={firstName.name}
+                label={firstName.label}
+                fullWidth
+              />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <InputField name={lastName.name} label={lastName.label} fullWidth />
+              <InputField
+                name={lastName.name}
+                label={lastName.label}
+                fullWidth
+              />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <SelectField
-                    name={gender.name}
-                    label={gender.label}
-                    data={genderlist}                    
-                    fullWidth
-                />
-            </Grid>
-            <Grid item xs={12} md={6}>         
-                <DatePickerField
-                    name={dateOfBirth.name}
-                    label={dateOfBirth.label}
-                    inputFormat="dd/MM/yyyy"  
-                    views={['day', 'month','year']}         
-                    minDate={new Date('1900/01/01')}
-                    maxDate={new Date('2006/01/01')}
-                    renderInput={(params) => (
-                        <TextField
-                        {...params}
-                        inputProps={{ ...params.inputProps, placeholder: "dd/mm/yyyy" }} />
-                    )}
-                    sx={{width: '100%'}}
-                />
+              <SelectField
+                name={gender.name}
+                label={gender.label}
+                data={genderlist}
+                fullWidth
+              />
             </Grid>
             <Grid item xs={12} md={6}>
-                <InputField name={aadharNumber.name} label={aadharNumber.label} fullWidth />
+              <DatePickerField
+                name={dateOfBirth.name}
+                label={dateOfBirth.label}
+                inputFormat="dd/MM/yyyy"
+                views={["day", "month", "year"]}
+                minDate={new Date("1900/01/01")}
+                maxDate={new Date("2006/01/01")}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    inputProps={{
+                      ...params.inputProps,
+                      placeholder: "dd/mm/yyyy",
+                    }}
+                  />
+                )}
+                sx={{ width: "100%" }}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <InputField
+                name={aadharNumber.name}
+                label={aadharNumber.label}
+                fullWidth
+              />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <InputField name={fatherSpouseName.name} label={fatherSpouseName.label} fullWidth />
+              <InputField
+                name={fatherSpouseName.name}
+                label={fatherSpouseName.label}
+                fullWidth
+              />
             </Grid>
             {/* <Grid item xs={12} sm={6}>
                 <SelectField
@@ -93,28 +117,48 @@ export default function EmpProfileForm(props) {
                 />
             </Grid> */}
             <Grid item xs={12} sm={6}>
-                <InputField name={nationality.name} label={nationality.label} value={countryName} fullWidth disabled />
+              <InputField
+                name={nationality.name}
+                label={nationality.label}
+                value={countryName}
+                fullWidth
+                disabled
+              />
             </Grid>
             <Grid item xs={12} sm={6}>
-                <InputField name={educationLevel.name} label={educationLevel.label} fullWidth />
+              <InputField
+                name={educationLevel.name}
+                label={educationLevel.label}
+                fullWidth
+              />
             </Grid>
-            <Grid item xs={12} md={6}>         
-                <DatePickerField
-                    name={dateOfJoning.name}
-                    label={dateOfJoning.label}
-                    inputFormat="dd/MM/yyyy"  
-                    views={['day', 'month','year']}         
-                    minDate={new Date()}
-                    maxDate={new Date()}
-                    renderInput={(params) => (
-                        <TextField
-                        {...params}
-                        inputProps={{ ...params.inputProps, placeholder: "dd/mm/yyyy" }} />
-                    )}
-                    sx={{width: '100%'}}
-                />
+            <Grid item xs={12} md={6}>
+              {/* <DatePickerField
+                name={dateOfJoning.name}
+                label={dateOfJoning.label}
+                inputFormat="dd/MM/yyyy"
+                views={["day", "month", "year"]}
+                minDate={new Date()}
+                maxDate={new Date()}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    slotProps={{
+                      ...params.inputProps,
+                      placeholder: "dd/mm/yyyy",
+                    }}
+                  />
+                )}
+                sx={{ width: "100%" }}
+              /> */}
+              <DatePickerField
+                 name={dateOfJoning.name}
+                 label={dateOfJoning.label}
+                defaultValue={moment("2022-04-17")}
+                slotProps={{ field: { shouldRespectLeadingZeros: true } }}
+              />
             </Grid>
           </Grid>
-          </React.Fragment>
+        </React.Fragment>
       );
 }
