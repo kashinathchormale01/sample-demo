6fff07ef68498a1cbba9d13469fb631a1c602462
kashinathchormale01 <chormalekashinath01@gmail.com>
2024-06-02 20:59:42 +0530

    seelect comp updated

--- src/component/EmployeeManagement/EmployeeEnrollment/SelectCompdep.js
+++ src/component/EmployeeManagement/EmployeeEnrollment/SelectCompdep.js
@@ -4,75 +4,73 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import {  useFormContext } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
 
-
-
-const SelectComp = ( {sendvalues,label,isRequired,sentname,sentdefaultvalue}) => {
-   const [age1, setAge] = React.useState("");
-  const {register,setValue}=useFormContext();
-  const controlname=String(sentname) ;
-  let objects =[];
-  objects= sendvalues;
+const SelectComp = ({
+  sendvalues,
+  label,
+  isRequired,
+  sentname,
+  sentdefaultvalue,
+}) => {
+  const [age1, setAge] = React.useState("");
+  const { register, setValue } = useFormContext();
+  const controlname = String(sentname);
+  let objects = [];
+  objects = sendvalues;
   const handleChange = (event) => {
-   //console.log("dep select opions are ",objects);
     setAge(event.target.value);
 
-    register(controlname,{required:false});
-      setValue(controlname,event.target.value);
- // console.log("values sent ",JSON.stringify(sendvalues))
- try {
-  const temp= sendvalues.find((ele)=>{
-    return ele.valueitem===event.target.value
-   
-  })
-  const altlbl=controlname+"1";
-  register(altlbl,{required:false});
-  setValue(altlbl,temp.labelitem,{shouldValidate:true});
-}catch(error){}
-
-
-
+    register(controlname, { required: false });
+    setValue(controlname, event.target.value);
+    try {
+      const temp = sendvalues.find((ele) => {
+        return ele.valueitem === event.target.value;
+      });
+      const altlbl = controlname + "1";
+      register(altlbl, { required: false });
+      setValue(altlbl, temp.labelitem, { shouldValidate: true });
+    } catch (error) {}
   };
   function setdefaultvalue() {
-   // console.log("deep selecct entered");
     let temp;
     try {
-       temp= sendvalues.find((ele)=>{
-        return ele.valueitem===sentdefaultvalue
-       
-      })
-    }catch(error){}
-  //  console.log("dep setdefault called",objects[0]?.valueitem,"and set value is",sentname);
-    if(temp!==undefined&&objects.length>0)
-      {
-        setAge(sentdefaultvalue);
-      }
-    else{
-    if(sentname==="roleId"&&objects.length>0)
-      {
-     //   console.log("dep2 setdefault called",objects[0]?.valueitem,"and set value is",sentname);
-      setAge('');
-      setAge(objects[0]?.valueitem);
+      temp = sendvalues.find((ele) => {
+        return ele.valueitem === sentdefaultvalue;
+      });
+    } catch (error) {}
+    console.log(
+      "dep setdefault called",
+      objects[0]?.valueitem,
+      "and set value is",
+      sentname
+    );
+    console.log("temp", temp);
+    if (temp !== undefined && objects.length > 0) {
+      setAge(sentdefaultvalue);
+    } else {
+      if (sentname === "roleId" && objects.length > 0) {
+        console.log("delayed", objects[0]?.valueitem);
+        setAge("");
+        setAge(objects[0]?.valueitem);
+        register(sentname, { required: false });
+        setValue(sentname, objects[0]?.valueitem, { shouldValidate: true });
+        const altlbl = controlname + "1";
+        register(altlbl, { required: false });
+        setValue(altlbl, objects[0]?.labelitem, { shouldValidate: true });
       }
     }
-    }
+  }
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     setdefaultvalue();
-   // if (sentdefaultvalue !== undefined) {
-   //   setAge(sentdefaultvalue);
-   //   setValue(sentname, sentdefaultvalue);
-   // }
-  },[objects])
-  
-  
+  }, [objects]);
+
   return (
     <Box sx={{ minWidth: 120 }}>
-     
-     <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">{label}</InputLabel>
-          <Select
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           defaultValue=""
@@ -80,19 +78,16 @@ const SelectComp = ( {sendvalues,label,isRequired,sentname,sentdefaultvalue}) =>
           label={label}
           required
           onChange={handleChange}
-         
-         
         >
-          {objects?.map((valueitem,index)=>{
-            return(
-                <MenuItem key={index} value={valueitem.valueitem}>{valueitem.labelitem}</MenuItem>
-            )
+          {objects?.map((valueitem, index) => {
+            return (
+              <MenuItem key={index} value={valueitem.valueitem}>
+                {valueitem.labelitem}
+              </MenuItem>
+            );
           })}
-        
         </Select>
-        </FormControl>
-       
-     
+      </FormControl>
     </Box>
   );
 };
